d677d74f4d7d3dffd8bbe371c31c6812c0ef02a8
aaronfox <aaron.m.fox3@gmail.com>
2020-02-21 11:37:12 -0500

    committing final adjustments

---
+++ Homework 2/Fox_Homework_2/Assets/Cog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cog : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Damage")
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Homework 2/Fox_Homework_2/Assets/Thwomp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thwomp : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fall if player is coming
+        if (transform.position.x - 2.5f <= player.position.x)
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+    }
+}
