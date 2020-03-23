2f26f134edb8e1eb8aa2d29f31cc88f6ed856e64
aaronfox <aaron.m.fox3@gmail.com>
2020-03-23 18:30:23 -0400

    latest work on zombie AI

---
+++ Fox_Project_4_FPS_Zombie/Assets/Zombie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+
+    public float radiusForAttacking = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radiusForAttacking);
+    }
+}
