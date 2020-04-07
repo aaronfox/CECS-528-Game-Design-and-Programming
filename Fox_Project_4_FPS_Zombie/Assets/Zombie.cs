14d76352998d6f48396568e288fc25120d368dfb
aaronfox <aaron.m.fox3@gmail.com>
2020-04-06 21:28:10 -0400

    have basic script for zombie to follow player

--- Fox_Project_4_FPS_Zombie/Assets/Zombie.cs
+++ Fox_Project_4_FPS_Zombie/Assets/Zombie.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    public float radiusForAttacking = 4.0f;
+    public Transform targetTransform;
+    public float speed = 1.0f;
+    float YPosition;
 
-    public float radiusForAttacking = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        YPosition = transform.position.y;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float distanceToPlayer = Vector3.Distance(targetTransform.position, transform.position);
+
+        // Basic following AI
+        if (distanceToPlayer <= radiusForAttacking)
+        {
+            transform.position = new Vector3(transform.position.x, YPosition, transform.position.z);
+            Vector3 targetPostition = new Vector3(targetTransform.position.x,
+                                       this.transform.position.y,
+                                       targetTransform.position.z);
+            this.transform.LookAt(targetPostition);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
     }
 
     private void OnDrawGizmosSelected()
